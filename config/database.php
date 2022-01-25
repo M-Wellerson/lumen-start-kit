13071ef45c123e53521109319f6650122c83b456
Marcos Wellerson <m_wellerson@outlook.com>
2022-01-25 10:14:33 -0300

    Update

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+$url = parse_url(env("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ]
+];
